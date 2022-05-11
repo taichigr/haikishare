18d1cae8451e783c1402955b0c26992bea6de241
taichigr <taichi.01gr14@gmail.com>
2022-05-11 23:49:47 +0900

    在庫管理機能作成　stocksテーブルのタイプについて調べる

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -16,5 +17,10 @@ class Product extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo('App\Models\Category');
+    }
+
+    public function stock(): HasMany
+    {
+        return $this->hasMany('App\Models\Stock');
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UsersTableSeeder::class,
             ShopsTableSeeder::class,
             ProductsTableSeeder::class,
+            StocksTableSeeder::class,
         ]);
     }
 }
