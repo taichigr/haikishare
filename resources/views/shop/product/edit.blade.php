0a406209c51b61721ec4305f2b1f192184244f6f
taichigr <taichi.01gr14@gmail.com>
2022-05-21 15:57:18 +0900

    購入済みかどうかで出品者が編集できるかの切り替え

--- resources/views/shop/product/edit.blade.php
+++ resources/views/shop/product/edit.blade.php
@@ -7,16 +7,20 @@
     </div>
 
     <div class="c-form__container">
+        @if($product->user_id == null)
         <form id="update" method="POST" action="{{ route('shop.product.update', ['product' => $product->id]) }}"
             enctype='multipart/form-data'>
             @method('PATCH')
             @csrf
+            @endif
             <div class="c-form__control">
                 <label for="name" class="c-form__label">商品名</label>
 
                 <div class="c-form__inputContainer">
                     <input id="name" type="text" class="c-form__input  @error('name') is-invalid @enderror" name="name"
-                        value="{{ $product->name ?? old('name') }}" required autocomplete="name">
+                        value="{{ $product->name ?? old('name') }}" required autocomplete="name" @if($product->user_id
+                    !==
+                    null) readonly @endif>
 
                     @error('name')
                     @include('../../components/error_message')
@@ -29,7 +33,8 @@
 
                 <div class="c-form__inputContainer">
                     <select name="category_id" id="category_id"
-                        class="c-form__select @error('name') is-invalid @enderror">
+                        class="c-form__select @error('name') is-invalid @enderror" @if($product->user_id !==
+                        null) disabled @endif>
                         <option value="" class="c-form__option">選択してください</option>
                         @foreach ($categories as $category)
                         <option value="{{ $category->id }}" class="c-form__option" @if($product->category_id ==
@@ -52,7 +57,9 @@
 
                 <div class="c-form__inputContainer">
                     <input id="price" type="number" class="c-form__inputNumber  @error('price') is-invalid @enderror"
-                        name="price" value="{{ $product->price ?? old('price') }}" required autocomplete="price">
+                        name="price" value="{{ $product->price ?? old('price') }}" required autocomplete="price"
+                        @if($product->user_id !==
+                    null) readonly @endif>
                     円
                     @error('price')
                     @include('../../components/error_message')
@@ -65,27 +72,35 @@
                 {{-- TODO 編集.ファイルをアップロード　アップロード時、バリデーション、アップロードの際にストレージに保存。axios --}}
                 <div class="c-form__inputContainer">
                     <input id="image" type="file" class="c-form__input  @error('image') is-invalid @enderror"
-                        name="image" value="{{ $product->image ?? old('image') }}" autocomplete="image">
+                        name="image" value="{{ $product->image ?? old('image') }}" autocomplete="image"
+                        @if($product->user_id
+                    !==
+                    null) disabled @endif>
                     @error('image')
                     @include('../../components/error_message')
                     @enderror
                 </div>
             </div>
 
-
+            @if($product->user_id == null)
         </form>
+        @endif
 
         <div class="c-form__buttonArea">
+            @if($product->user_id !== null)
+            *商品が購入されたため、編集・削除ができません。
+            @else
             <button form="delete" type="submit" class="c-button__default--alert" onclick="return confirm('本当に削除しますか？')">
                 商品削除
             </button>
             <button form="update" type="submit" class="c-button__default">
-                商品登録
+                編集完了
             </button>
             <form id="delete" method="POST" action="{{route('shop.product.destroy', ['product' => $product->id])}}">
                 @method('DELETE')
                 @csrf
             </form>
+            @endif
         </div>
     </div>
 </div>
