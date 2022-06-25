8c7b806ce6341da8d1b00cfd435c8888432d77ae
taichigr <taichi.01gr14@gmail.com>
2022-06-25 23:12:46 +0900

    trustproxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies=[
+        '172.16.3.11/24'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
