2453357bb7aa5bcb3131eb8410af57ef6c7772eb
taichigr <taichi.01gr14@gmail.com>
2022-07-07 23:17:56 +0900

    inputからpタグ

--- resources/views/shop/product/detail.blade.php
+++ resources/views/shop/product/detail.blade.php
@@ -25,9 +25,7 @@
             <label for="name" class="c-form__label">商品名</label>
 
             <div class="c-form__inputContainer">
-                <input id="name" type="text" class="c-form__input" name="name"
-                    value="{{ $product->name ?? old('name') }}" required autocomplete="name" readonly>
-
+                <p>{{ $product->name ?? old('name') }}</p>
 
             </div>
         </div>
@@ -36,17 +34,11 @@
             <label for="category_id" class="c-form__label">商品カテゴリー</label>
 
             <div class="c-form__inputContainer">
-                <select name="category_id" id="category_id" class="c-form__select" disabled>
-                    <option value="" class="c-form__option">選択してください</option>
-                    @foreach ($categories as $category)
-                    <option value="{{ $category->id }}" class="c-form__option" @if($product->category_id ==
-                        $category->id)
-                        selected
-                        @else
-                        @if(old('category_id')==$category->id) selected @endif
-                        @endif>{{ $category->name }}</option>
-                    @endforeach
-                </select>
+                @foreach ($categories as $category)
+                    @if($product->category_id == $category->id)
+                    <p>{{ $category->name }}</p>
+                    @endif
+                @endforeach
 
 
             </div>
@@ -56,15 +48,19 @@
             <label for="price" class="c-form__label">価格</label>
 
             <div class="c-form__inputContainer">
-                <input id="price" type="number" class="c-form__inputNumber" name="price"
-                    value="{{ $product->price ?? old('price') }}" required autocomplete="price" readonly>
-                円
-
+                <p>{{$product->price}}円</p>
             </div>
         </div>
 
         <div class="c-form__control">
-            <label for="image" class="c-form__label">画像</label>
+            <label for="price" class="c-form__label">賞味期限</label>
+
+            <div class="c-form__inputContainer">
+                <p>{{$product->expired_at}}</p>
+            </div>
+        </div>
+
+        <div class="c-form__control">
             <div class="c-form__inputContainer">
                 @if($product->image)
                 <img src="{{ asset('storage/uploads/products/'.$product->image) }}" alt="{{$product->name}}">
@@ -73,7 +69,6 @@
                 @endif
             </div>
         </div>
-
 
 
     </div>
